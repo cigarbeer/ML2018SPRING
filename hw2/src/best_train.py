129bfabefa1f5c78976fd4c9c101cdc7eb01f4b9
cigarbeer <r06921038@ntu.edu.tw>
2018-04-01 22:12:30 +0800

    ok

--- hw2/src/best_train.py
+++ hw2/src/best_train.py
@@ -4,9 +4,21 @@ from util import *
 import sys 
 import best_setting  as st 
 
+from sklearn.linear_model import LogisticRegressionCV 
+from sklearn.covariance import EllipticEnvelope 
+from sklearn.preprocessing import scale 
+from sklearn.svm import SVC
+from sklearn.feature_selection import RFE 
 
 
 if __name__ == '__main__':
     X, y = read_training_data(st.TRAINGING_X_PATH, st.TRAINGING_Y_PATH)
 
+    X = scale(X) 
+
+    ee = EllipticEnvelope()
+    ee.fit(X, y.flatten())
+
+    inliers_mask = ee.predict(X)
     
+

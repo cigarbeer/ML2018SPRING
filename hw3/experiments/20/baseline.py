0ac44c38bcb1e3dedbd678d85f9a1863b4574a86
cigarbeer <r06921038@ntu.edu.tw>
2018-04-17 23:58:12 +0800

    rep 20 aug

--- hw3/experiments/20/baseline.py
+++ hw3/experiments/20/baseline.py
@@ -283,6 +283,7 @@ if __name__ == '__main__':
     model = net(input_shape=IMAGE_SHAPE, output_shape=OUTPUT_CLASSES_NUM)
     model = compile_model(model)
     model = fit_generator(model, X, y, epochs=200, batch_size=BATCH_SIZE, model_saving_path='./')
+    model.save('after_best.hdf5')
     idx, t = read_raw_testing_data('../../dataset/test.csv')
     t = preprocess_testing_data(t, mu, sigma) 
     pred = predict(model, t, batch_size=BATCH_SIZE)

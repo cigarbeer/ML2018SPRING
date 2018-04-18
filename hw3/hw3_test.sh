51f239b096f5e32df66db49eb56c768a47f35d86
cigarbeer <r06921038@ntu.edu.tw>
2018-04-18 20:51:19 +0800

    dropbox

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -2,6 +2,6 @@ testing_data=$1
 prediction_file=$2 
 mode=$3 
 
-wget https://github.com/cigarbeer/ML2018SPRING/releases/download/v1.0/model.hdf5
+wget 'https://www.dropbox.com/s/4bjqpzyvlxj89r3/model.hdf5?dl=1' -O 'model.hdf5'
 
 python3 test.py $testing_data $prediction_file 

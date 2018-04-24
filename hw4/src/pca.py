9133367a04ef4f4a9eba2310d30ede5522da71c1
cigarbeer <r06921038@ntu.edu.tw>
2018-04-24 20:32:59 +0800

    hw4 pca

--- hw4/src/pca.py
+++ hw4/src/pca.py
@@ -2,8 +2,8 @@ import numpy as np
 import skimage.io 
 import sys 
 
-def read_data(file_path): 
-    return np.load(file_path)
+def read_data(file_path, debug=False): 
+    return np.load(file_path) if not debug else np.load(file_path)[:1000]
 
 def normalize(X): 
     mu, sigma = statistics(X) 
@@ -12,23 +12,27 @@ def normalize(X):
 
 def denormalize(N, mu, sigma): 
     X = N * sigma + mu 
-    return X 
+    return X.astype(np.uint8)
 
 def statistics(X): 
     mu = np.mean(X, axis=0) 
     sigma = np.std(X, axis=0) + 1e-10
     return mu, sigma 
 
-def svd(X): 
-    C = np.cov(X.T) 
+def cov(X):
+    return np.cov(X.T) 
+
+def svd(C): 
     U, S, V = np.linalg.svd(C)
     return U, S, V 
 
 def reduce_dimension(X, U, k): 
     return np.dot(X, U[:, :k]) 
 
-def reconstruct(Z, U, k): 
+def recover_dimension(Z, U, k): 
     return np.dot(Z, U[:, :k].T) 
+
+
 
 if __name__ == '__main__': 
     data_file_path = sys.argv[1] 

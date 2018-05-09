5771a20864444d51a3ae6ef25b756efe86b9deb1
cigarbeer <r06921038@ntu.edu.tw>
2018-05-10 00:28:48 +0800

    ut

--- hw5/util.py
+++ hw5/util.py
@@ -1,8 +1,16 @@
 import numpy as np 
 import pandas as pd 
+import logging 
+
+import gensim 
+from keras.preprocessing.text import Tokenizer 
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO) 
+
+STOPWORDS = set('for a an of the and to in on at or are am'.split(' '))
 
 def read_label_data(path):
-    data = pd.read_table(path, header=None, names=['label', 'words'], sep='\+\+\+\$\+\+\+', engine='python') 
-    data.words = data.words.str.strip().str.lower() 
-    return data.label, data.words  
+    data = pd.read_table(path, header=None, names=['label', 'words'], sep='\+\+\+\$\+\+\+', encoding='utf-8', engine='python') 
+    return data.label, data.words.str.strip().str.lower() 
 
+# def bag_of_words()

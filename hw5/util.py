4e786d9d0db9fc87051340853d30e492f5fbe019
cigarbeer <r06921038@ntu.edu.tw>
2018-05-09 20:57:34 +0800

    ut

--- hw5/util.py
+++ hw5/util.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd 
 
 def read_label_data(path):
-    data = pd.read_table(path, header=None, names=['label', 'words'], sep='\+\+\+\$\+\+\+')
+    data = pd.read_table(path, header=None, names=['label', 'words'], sep='\+\+\+\$\+\+\+', engine='python') 
+    data.words = data.words.str.strip().str.lower() 
     return data.label, data.words  
 

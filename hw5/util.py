6c090f7b7b5fde6459c7ec6f37dff1df955fd1c2
cigarbeer <r06921038@ntu.edu.tw>
2018-05-09 18:15:21 +0800

    ut

--- hw5/util.py
+++ hw5/util.py
@@ -2,6 +2,6 @@ import numpy as np
 import pandas as pd 
 
 def read_label_data(path):
-    data = pd.read_table(path, sep=' +++$+++ ')
-    return data 
+    data = pd.read_table(path, header=None, names=['label', 'words'], sep='\+\+\+\$\+\+\+')
+    return data.label, data.words  
 

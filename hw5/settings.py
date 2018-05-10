b6cdc96903b1abc073f69471fb429611718115c5
cigarbeer <r06921038@ntu.edu.tw>
2018-05-11 00:59:14 +0800

    ut st

---
+++ hw5/settings.py
@@ -0,0 +1,18 @@
+MAX_DICTIONATY_SIZE = 50000 
+
+
+STOPWORDS = set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you',
+       'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his',
+       'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself',
+       'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which',
+       'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
+       'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had',
+       'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and',
+       'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at',
+       'by', 'for', 'with', 'about', 'against', 'between', 'into',
+       'through', 'during', 'before', 'after', 'above', 'below', 'to',
+       'from', 'up', 'down', 'in', 'out', 'on', 'off', 'under',
+       'again', 'further', 'then', 'once', 'here', 'there', 'when',
+       'where', 'all', 'both', 'each', 'few', 'more',
+       'most', 'other', 'some', 'own',
+       'same', 'so', 'than', 's', 't', 'now'])
--- hw5/util.py
+++ hw5/util.py
@@ -11,10 +11,11 @@ from keras.models import Sequential
 from keras.models import save_model 
 from keras.models import load_model 
 
+from settings import STOPWORDS 
+from settings import MAX_DICTIONATY_SIZE 
+
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO) 
 
-STOPWORDS = set('for a an of the and to as in on at or are i am was 2'.split(' '))
-MAX_DICTIONATY_SIZE = 10000 
 
 
 def read_label_data(path):
@@ -23,18 +24,37 @@ def read_label_data(path):
 
 def read_unlabel_data(path):
     data = pd.read_table(path, header=None, names=['texts'], sep='\r\n', encoding='utf-8', engine='python') 
-    return data.texts.str.strip().str.lower()
+    return data.texts.str.strip().str.lower() 
+
+def read_testing_data(path): 
+    return 
 
 def concat_data(label_texts, unlabel_texts): 
     return pd.concat([label_texts, unlabel_texts]) 
 
+def split_texts(texts):
+    return texts.apply(text_to_word_sequence, lower=True, split=' ')
+
 def texts2corpus(texts): 
     return split_texts(texts) 
 
-def build_dictionary(corpus, max_dct_size): 
+def texts2bow(texts, dct): 
+    corpus = texts2corpus(texts) 
+    bow = corpus2bow(corpus, dct) 
+    return bow 
+
+def corpus2bow(corpus, dct): 
+    bow = [dct.doc2bow(doc) for doc in corpus] 
+    return bow 
+
+def bow2dense(bow, dct): 
+    dense = gensim.matutils.corpus2dense(bow, num_terms=len(dct)) 
+    return dense.T 
+
+def build_dictionary(corpus, min_count, max_dct_size): 
     dct = Dictionary(corpus) 
     dct.filter_extremes(
-        no_below=3, 
+        no_below=min_count, 
         no_above=0.5, 
         keep_n=max_dct_size, 
         keep_tokens=None 
@@ -47,6 +67,7 @@ def build_dictionary(corpus, max_dct_size):
     dct.compactify() 
     return dct 
 
+
 def tokenize(words, num_words): 
     tkn = Tokenizer(
         num_words=num_words, 
@@ -58,8 +79,6 @@ def tokenize(words, num_words):
     tkn.fit_on_texts(words) 
     return tkn 
 
-def split_texts(texts):
-    return texts.apply(text_to_word_sequence, lower=True, split=' ')
 
 def word2vec(corpus, dim, window, min_count, n_iter): 
     return gensim.models.Word2Vec(
@@ -85,5 +104,4 @@ def load_object(path):
         obj = pickle.load(f) 
         f.close()
     return obj 
-
 

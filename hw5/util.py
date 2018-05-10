e8cfd06238a0bb2d59501c51195a0c65aec8cafe
cigarbeer <r06921038@ntu.edu.tw>
2018-05-10 21:12:25 +0800

    ut

--- hw5/util.py
+++ hw5/util.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import pandas as pd 
 import logging 
-
+from string import punctuation 
 import gensim 
 from keras.preprocessing.text import Tokenizer 
 from keras.preprocessing.text import text_to_word_sequence 
@@ -11,7 +11,9 @@ from keras.models import load_model
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO) 
 
-STOPWORDS = set('for a an of the and to in on at or are am'.split(' '))
+STOPWORDS = set('for a an of the and to in on at or are i am'.split(' '))
+MAX_DICTIONATY_SIZE = 10000 
+
 
 def read_label_data(path):
     data = pd.read_table(path, header=None, names=['label', 'words'], sep='\+\+\+\$\+\+\+', encoding='utf-8', engine='python') 
@@ -24,10 +26,23 @@ def read_unlabel_data(path):
 def concat_data(label_data, unlabel_data): 
     return pd.concat([label_data, unlabel_data]) 
 
-def split_words(words):
-    return words.apply(text_to_word_sequence)
+def tokenize(words, num_words): 
+    tkn = Tokenizer(
+        num_words=num_words, 
+        filters=punctuation, 
+        lower=True, 
+        split=' ', 
+        char_level=False 
+    ) 
+    tkn.fit_on_texts(words) 
+    return tkn 
 
-def word2vec(words, dim, window, min_count, n_iter): 
-    return gensim.models.Word2Vec(sentences=words, size=dim, window=window, min_count=min_count, iter=n_iter, compute_loss=True, seed=0) 
+# def split_words(words):
+#     return words.apply(text_to_word_sequence)
+
+# def word2vec(words, dim, window, min_count, n_iter): 
+#     return gensim.models.Word2Vec(sentences=words, size=dim, window=window, min_count=min_count, iter=n_iter, compute_loss=True, seed=0) 
+
+
 
 

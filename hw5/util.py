71e94005e4da3bc803adf3e33fbea86717a04413
cigarbeer <r06921038@ntu.edu.tw>
2018-05-10 20:10:04 +0800

    ut

--- hw5/util.py
+++ hw5/util.py
@@ -4,6 +4,10 @@ import logging
 
 import gensim 
 from keras.preprocessing.text import Tokenizer 
+from keras.preprocessing.text import text_to_word_sequence 
+from keras.models import Sequential 
+from keras.models import save_model 
+from keras.models import load_model 
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO) 
 
@@ -16,3 +20,14 @@ def read_label_data(path):
 def read_unlabel_data(path):
     data = pd.read_table(path, header=None, names=['words'], sep='\r\n', encoding='utf-8', engine='python') 
     return data.words.str.strip().str.lower()
+
+def concat_data(label_data, unlabel_data): 
+    return pd.concat([label_data, unlabel_data]) 
+
+def split_words(words):
+    return words.apply(text_to_word_sequence)
+
+def word2vec(words, dim, window, min_count, n_iter): 
+    return gensim.models.Word2Vec(sentences=words, size=dim, window=window, min_count=min_count, iter=n_iter, compute_loss=True, seed=0) 
+
+

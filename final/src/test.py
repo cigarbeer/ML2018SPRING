1ace5030501025584ed51503f2a26517d6f6f66f
cigarbeer <r06921038@ntu.edu.tw>
2018-05-16 00:01:47 +0800

    src

--- final/src/test.py
+++ final/src/test.py
@@ -3,6 +3,7 @@ from . import data as D
 from keras.models import load_model 
 
 import numpy as np 
+import pandas as pd 
 
 import sys 
 import os 
@@ -16,8 +17,16 @@ def predict(model, generator):
         use_multiprocessing=False, 
         verbose=1 
     ) 
-    return predicted_probability 
+    predicted_probability_sorted_reverse = np.argsort(predicted_probability, axis=1)[:, :-6:-1] 
+    return predicted_probability_sorted_reverse  
 
+def get_idx2class(class_indices): 
+    idx2class = np.empty(shape=(len(class_indices),), dtype='object') 
+    for class_name, idx in class_indices.items():
+        idx2class[idx] = class_name 
+    return idx2class 
+
+# def write_prediction(prediction_idx, output_file, idx2class, ): 
 
 if __name__ == '__main__': 
     model_path = sys.argv[1] 
@@ -26,5 +35,11 @@ if __name__ == '__main__':
     prediction_output_file = sys.argv[4] 
 
     model = load_model(model_path) 
-    predicted_probability = predict(model, D.get_test_generator(directory=testing_set_dir)) 
-    prediction = np.argmax(predicted_probability, axis=1) 
+    idx2class = get_idx2class(D.get_train_generator(directory=training_set_dir).class_indices) 
+    test_generator = D.get_test_generator(directory=testing_set_dir)
+    prediction_idx = predict(model, test_generator) 
+    prediction_class_name = idx2class[prediction_idx] 
+    class_name_str = pd.DataFrame(prediction_class_name).apply(lambda x: ' '.join(x), axis=1) 
+    image_name_str = pd.DataFrame(test_generator.filenames).apply(lambda x: os.path.basename(x)) 
+    df = pd.concat([image_name_str, class_name_str], names=['Image', 'Id']) 
+    df.to_csv(prediction_output_file)

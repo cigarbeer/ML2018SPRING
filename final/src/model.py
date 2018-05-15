91b4a9d7e08711911633edd702980bd1a10c69b5
cigarbeer <r06921038@ntu.edu.tw>
2018-05-15 20:31:12 +0800

    src

--- final/src/model.py
+++ final/src/model.py
@@ -15,17 +15,19 @@ from keras.callbacks import ModelCheckpoint
 
 from keras.applications.vgg16 import VGG16 
 
+vgg16 = VGG16(input_tensor=input_layer, weights='imagenet', include_top=False)  
+
 input_layer = Input(shape=(224, 224, 3), name='input_layer') 
-vgg16 = VGG16(input_tensor=input_layer, weights='imagenet', include_top=False)(input_layer) 
+vgg16_block = vgg16(input_layer) 
 global_average_pooling_layer = GlobalAveragePooling2D(name='global_average_pooling_layer')(vgg16.output) 
 
-dense_layer_1 = Dense(units=256, activation='selu')(global_average_pooling_layer) 
-batch_normalization_layer_1 = BatchNormalization()(dense_layer_1) 
-dropout_layer_1 = Dropout(rate=0.25)(batch_normalization_layer_1)
+dense_layer_1 = Dense(units=256, activation='selu', name='dense_layer_1')(global_average_pooling_layer) 
+batch_normalization_layer_1 = BatchNormalization(name='batch_normalization_layer_1')(dense_layer_1) 
+dropout_layer_1 = Dropout(rate=0.25, name='dropout_layer_1')(batch_normalization_layer_1)
 
-dense_layer_2 = Dense(units=256, activation='selu')(dropout_layer_1) 
-batch_normalization_layer_2 = BatchNormalization()(dense_layer_2) 
-dropout_layer_2 = Dropout(rate=0.25)(batch_normalization_layer_2)
+dense_layer_2 = Dense(units=256, activation='selu', name='dense_layer_2')(dropout_layer_1) 
+batch_normalization_layer_2 = BatchNormalization(name='batch_normalization_layer_2')(dense_layer_2) 
+dropout_layer_2 = Dropout(rate=0.25, name='dropout_layer_2')(batch_normalization_layer_2)
 output_layer = Dense(units=4251, activation='softmax', name='output_layer')(dropout_layer_2) 
 model = Model(inputs=input_layer, outputs=output_layer)
 

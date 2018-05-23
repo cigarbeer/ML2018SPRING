9cb79c20cc11a58baea16ef22bae878c351dabbd
cigarbeer <r06921038@ntu.edu.tw>
2018-05-23 16:47:36 +0800

    test

--- hw5/hw5_test.sh
+++ hw5/hw5_test.sh
@@ -2,4 +2,7 @@ testingData=$1
 predictionFile=$2 
 mode=$3 
 
-python3 test.py $testingData $predictionFile 
+wget 'https://www.dropbox.com/s/9n6n0fakfwfdoyy/punc.final.hdf5?dl=1' -O 'punc.hdf5' 
+wget 'https://www.dropbox.com/s/gumnc4wh9tvk0ji/hw5.punc.pickle?dl=1' -O 'hw5.pickle' 
+
+python3 punc.py $testingData $predictionFile 

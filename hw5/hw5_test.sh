22d24a088ea856160c0020ed757de08f9e7c23c2
cigarbeer <r06921038@ntu.edu.tw>
2018-05-23 16:51:36 +0800

    test

--- hw5/hw5_test.sh
+++ hw5/hw5_test.sh
@@ -5,4 +5,4 @@ mode=$3
 wget 'https://www.dropbox.com/s/9n6n0fakfwfdoyy/punc.final.hdf5?dl=1' -O 'punc.hdf5' 
 wget 'https://www.dropbox.com/s/gumnc4wh9tvk0ji/hw5.punc.pickle?dl=1' -O 'hw5.pickle' 
 
-python3 punc.py $testingData $predictionFile 
+python3 punc_test.py $testingData $predictionFile 

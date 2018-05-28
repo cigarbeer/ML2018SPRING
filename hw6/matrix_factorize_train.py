d4e77e8244cdeff5d8c35f1e494065bc694e9020
cigarbeer <r06921038@ntu.edu.tw>
2018-05-28 20:05:41 +0800

    mf

--- hw6/matrix_factorize_train.py
+++ hw6/matrix_factorize_train.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     (n_users,) = uid.unique().shape 
     (n_movies,) = mid.unique().shape 
     print('[build matrix factorization model]')
-    model = get_matrix_factorization_model(matrix_shape=(n_users, n_movies), LATENT_DIMENSION) 
+    model = get_matrix_factorization_model(matrix_shape=(n_users, n_movies), latent_dimension=LATENT_DIMENSION) 
     print('[start training]')
     train(model, X=[uid, mid], y=rating_normalized, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_split=VALIDATION_SPLIT, model_checkpoint_path=MATRIX_FACTORIZATION_CHECKPOINT_PATH) 
     print('[done]') 

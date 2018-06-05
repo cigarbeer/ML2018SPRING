9a7408d24c2d2fe757697d97acb51a1e6adcfef0
cigarbeer <r06921038@ntu.edu.tw>
2018-06-05 22:50:35 +0800

    p5

--- hw6/report/p5/p5.py
+++ hw6/report/p5/p5.py
@@ -14,6 +14,7 @@ from keras.layers import Embedding
 from keras.layers import Flatten 
 from keras.layers import Concatenate 
 from keras.layers import Dropout 
+from keras.layers import BatchNormalization 
 
 from keras.callbacks import EarlyStopping 
 from keras.callbacks import ModelCheckpoint 
@@ -49,7 +50,58 @@ def read_movies(path):
             genres_onehot[row['movieID']][genres_dct[g]] = 1.0 
     return genres_onehot, genres_dct  
 
+def read_training(path): 
+    df = pd.read_csv(path) 
+    df = df.sample(frac=1, replace=False) 
+    uid = df.UserID.astype(np.int32) 
+    mid = df.MovieID.astype(np.int32) 
+    rating = df.Rating.astype(np.float32) 
+    return uid, mid, rating 
 
+def make_training(uid, mid, users, movies): 
+    train = [] 
+    for u, m in zip(uid, mid): 
+        train.append(np.concatenate([users[u], movies[m]])) 
+    return train 
 
-    
-    
+def get_model(input_shape):
+    input_layer = Input(shape=input_shape) 
+    x = Dense(256, activation='selu')(input_layer) 
+    x = BatchNormalization()(x) 
+    x = Dropout(0.5)(x) 
+    x = Dense(128, activation='selu')(x) 
+    x = BatchNormalization()(x) 
+    x = Dropout(0.5)(x)
+    x = Dense(64), activation='selu'(x) 
+    x = BatchNormalization()(x) 
+    x = Dropout(0.5)(x)
+    x = Dense(128, activation='selu')(x) 
+    x = BatchNormalization()(x) 
+    x = Dropout(0.5)(x)
+    x = Dense(256, activation='selu')(x) 
+    x = BatchNormalization()(x) 
+    x = Dropout(0.5)(x)
+    output_layer = Dense(1, activation='relu')(x) 
+
+    model = Model(inputs=[input_layer], outputs=[output_layer]) 
+    model.compile(optimizer='nadam', loss='mse') 
+    model.summary()
+    return model 
+
+def train(model, X, y): 
+    callbacks = [ 
+        EarlyStopping(monitor='val_loss', min_delta=1e-4, patience=10, verbose=1), 
+        ModelCheckpoint(filepath='./p5.hdf5', monitor='val_loss', save_best_only=True, save_weights_only=False, verbose=1) 
+    ]
+    model.fit(
+        x=X, 
+        y=y, 
+        batch_size=512, 
+        epochs=100, 
+        callbacks=callbacks, 
+        validation_split=0.1, 
+        shuffle=True, 
+        verbose=1 
+    ) 
+    return model 
+
